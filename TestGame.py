804720ec342807f6784ca13ce08ecb62be73581e
jiaweiw02 <wuj22@rpi.edu>
2023-03-16 23:15:55 -0400

    adding smart contract

--- TestGame.py
+++ TestGame.py
@@ -1,4 +1,27 @@
 import pygame
+import json
+from web3 import Web3, HTTPProvider
+
+# setup dependencies for smart contract
+import json
+from web3 import Web3, HTTPProvider
+
+# dependencies
+blockchainAddress = "HTTP://127.0.0.1:7545"
+web3 = Web3(HTTPProvider(blockchainAddress))
+compiledContractPath = "build/contracts/Auction.json"
+deployedContractAddress = "X"
+with open(compiledContractPath) as file:
+    contract_json = json.load(file)
+    # contract abi is used to call the contract's functions
+    contract_abi = contract_json['abi']
+
+# reference deployed contract
+contract = web3.eth.contract(address=deployedContractAddress, abi=contract_abi)
+
+# function calls
+
+# -------------------------------------------------------------------------------
 
 # Initialize Pygame
 pygame.init()
